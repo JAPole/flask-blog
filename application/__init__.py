9ed47c4b286ecd6b2306d3973ef99507f7395a2e
JAPole <yvr71@hotmail.co.uk>
2020-03-11 16:48:20 +0000

    working code app inc logout

--- application/__init__.py
+++ application/__init__.py
@@ -2,9 +2,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
+
+
+bcrypt = Bcrypt(app)
+
+
+login_manager = LoginManager(app)
+
+login_manager.login_view = 'login'
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+pymysql://' + getenv('MYSQL_USER') + ':' + getenv('MYSQL_PASSWORD') + '@' + getenv('MYSQL_HOST') + '/' + getenv('MYSQL_DB')
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -16,5 +26,5 @@ app.config['SQLALCHEMY_DATABASE_URI']=getenv('DATABASE_URI')
 db = SQLAlchemy(app)
 
 
-# import the ./application/routes.py file
+
 from application import routes
